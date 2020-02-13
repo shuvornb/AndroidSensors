3abcbaf99be2db670ebfcb6bd66cc3483c3eca0d
shuvornb <shuvornb@gmail.com>
2020-02-13 16:55:22 -0500

    Initial commit

---
+++ app/src/main/java/com/ecit/androidsensors/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.ecit.androidsensors
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.content_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.content_main)
+
+        btnShowData.setOnClickListener {
+
+        }
+    }
+}
